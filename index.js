f743e426772ad131c53b5c8ecd4715db7d45f6bf
Oroko <aoroko09@gmail.com>
2021-05-01 15:53:01 -0400

    commit tree cc7a85197de7c962ffd17434110c3e7dd632c75c

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your functions here
+// const map = (arr, fn) => {
+//   return arr.map((n) => fn(n));
+// };
+
+const map = (arr,fn) => {
+  let newArr = []
+  arr.forEach(n => {
+    newArr.push(fn(n))
+  })
+  return newArr
+}
+// const reduce = (arr, fn, startingPoint=0) => {
+//   return arr.reduce((acc, currentValue) => {
+//     return fn(acc, currentValue);
+//   }, startingPoint);
+// };
+
+const reduce = (arr, fn, startingPoint=0) => {
+  let acc = startingPoint
+  for(const val of arr){
+    acc = fn(acc, val)
+  }
+  return acc
+}
